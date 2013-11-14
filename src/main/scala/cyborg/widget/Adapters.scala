a386ac136d45813fd5d56481fedae5f7808dd031
fgeldenhuys <manifesto.za@gmail.com>
2013-11-14 07:59:35 +0200

    more work on adapters

--- src/main/scala/cyborg/widget/Adapters.scala
+++ src/main/scala/cyborg/widget/Adapters.scala
@@ -12,25 +12,44 @@ object Adapters {
     def apply(source: A): BaseAdapter
   }
 
+  trait ListAdapterMaker[A] extends AdapterMaker[List[A]] {
+    def apply(source: List[A]) = new BaseAdapter {
+      def getCount: Int = source.size
+      def getItem(i: Int): AnyRef = source(i).asInstanceOf[AnyRef]
+      def getItemId(id: Int): Long = id
+      def getView(i: Int, convert: View, parent: ViewGroup): View =
+        makeView(source(i), convert, parent)
+    }
+    def makeView(item: A, convert: View, parent: ViewGroup): View
+  }
+
   def makeAdapter[A](source: A)(implicit adapterMaker: AdapterMaker[A]): BaseAdapter =
     adapterMaker(source)
 
   def makeListAdapterMapping[A](vr: ViewResource[TextView])
                               (implicit s: Show[A], context: Context) = {
-    new AdapterMaker[List[A]] {
-      def apply(source: List[A]) = new BaseAdapter {
-        def getCount: Int = source.size
-        def getItem(i: Int): AnyRef = source(i).asInstanceOf[AnyRef] //yuck
-        def getItemId(id: Int): Long = id
-        def getView(i: Int, convert: View, parent: ViewGroup): View = {
-          val view =
-            if (convert != null) convert.asInstanceOf[TextView]
-            else vr.inflate(parent)(context)
-          view.text = source(i).shows
-          view
-        }
+    new ListAdapterMaker[A] {
+      def makeView(item: A, convert: View, parent: ViewGroup): View = {
+        val view =
+          if (convert != null) convert.asInstanceOf[TextView]
+          else vr.inflate(parent)(context)
+        view.text = item.shows
+        view
       }
     }
   }
 
+  def makeListAdapterMapping[A](f: (A, View, ViewGroup) => View)
+                               (implicit context: Context) = {
+    new ListAdapterMaker[A] {
+      def makeView(item: A, convert: View, parent: ViewGroup): View = f(item, convert, parent)
+    }
+  }
+
+  val emptyAdapter = new BaseAdapter {
+    def getCount: Int = 0
+    def getItem(i: Int): AnyRef = null
+    def getItemId(id: Int): Long = id
+    def getView(i: Int, convert: View, parent: ViewGroup): View = null
+  }
 }
--- src/main/scala/cyborg/widget/Events.scala
+++ src/main/scala/cyborg/widget/Events.scala
@@ -4,20 +4,23 @@ import android.text.{Editable, TextWatcher}
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.AdapterView
-import android.widget.AdapterView.OnItemClickListener
+import scalaz._, Scalaz._
 
 object Events {
-  implicit class ViewEvents(val view: android.view.View) {
+  implicit class ViewEvents(val view: android.view.View) extends AnyVal {
     def onClick(f: => Any) { view.setOnClickListener(new OnClickListener {
       def onClick(v: View) { f }
     })}
   }
-  /*implicit class ButtonEvents(val button: android.widget.Button) {
-    def onClick(f: => Any) { button.setOnClickListener(new OnClickListener {
-      def onClick(v: View) { f }
+
+  implicit class AdapterViewEvents(val av: AdapterView[_]) extends AnyVal {
+    def onItemSelected(f: (Option[Int]) => Any) { av.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener {
+      def onItemSelected(parent: AdapterView[_], view: View, i: Int, id: Long) { f(i.some) }
+      def onNothingSelected(parent: AdapterView[_]) { f(none) }
     })}
-  }*/
-  implicit class TextViewEvents(val textView: android.widget.TextView) {
+  }
+
+  implicit class TextViewEvents(val textView: android.widget.TextView) extends AnyVal {
     def onTextChanged(f: (CharSequence) => Any) { textView.addTextChangedListener(new TextWatcher {
       val textChangedFun = (text: CharSequence) => f(text)
       def beforeTextChanged(p1: CharSequence, p2: Int, p3: Int, p4: Int) {}
@@ -25,8 +28,9 @@ object Events {
       def afterTextChanged(p1: Editable) {}
     })}
   }
-  implicit class ListViewEvents(val list: android.widget.ListView) {
-    def onItemClick(f: (Int) => Any) { list.setOnItemClickListener(new OnItemClickListener {
+
+  implicit class ListViewEvents(val list: android.widget.ListView) extends AnyVal {
+    def onItemClick(f: (Int) => Any) { list.setOnItemClickListener(new AdapterView.OnItemClickListener {
       def onItemClick(parent: AdapterView[_], view: View, i: Int, id: Long) { f(i) }
     })}
   }
