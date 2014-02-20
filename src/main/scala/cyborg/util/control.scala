fgeldenhuys/cyborg

commit 69d3a2475bb5383b8745c521b78fbcd112d0701c

Prefer combinators for tryOption
Signed-off-by: Gary Pamparà <gpampara@gmail.com>

src/main/scala/cyborg/util/control.scala
@@ -22,13 +22,10 @@ object control {
   }
 
   def tryOption[T](f: => T): Option[T] = {
-    try {
-      Option(f)
-    } catch {
-      case e: Throwable =>
-        e.printStackTrace()
-        None
-    }
+    \/.fromTryCatch(f).fold(l => {
+      l.printStackTrace
+      None
+    }, Some(_))
   }
 
   def tryElse[T](f: => T)(e: Exception => T) = handling(classOf[Exception])

commit 2d409f44254397219d456599248207d32a30f307

fix bug caused by 69d3a2475bb5383b8745c521b78fbcd112d0701c tryOption should check for null

src/main/scala/cyborg/util/control.scala
@@ -25,7 +25,7 @@ object control {
     \/.fromTryCatch(f).fold(l => {
       l.printStackTrace
       None
-    }, Some(_))
+    }, Option(_))
   }
 
   def tryElse[T](f: => T)(e: Exception => T) = handling(classOf[Exception])