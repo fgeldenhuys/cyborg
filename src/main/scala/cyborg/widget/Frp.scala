317d9153507f0fbcc62861d6e40052c3e299e9f0
fgeldenhuys <manifesto.za@gmail.com>
2014-06-19 12:39:36 +0200

    add textChanges frp event stream

--- src/main/scala/cyborg/widget/Frp.scala
+++ src/main/scala/cyborg/widget/Frp.scala
@@ -1,5 +1,6 @@
 package cyborg.widget
 
+import android.text.{TextWatcher, Editable}
 import io.dylemma.frp._
 
 object Frp {
@@ -8,6 +9,27 @@ object Frp {
     val es = EventSource[Unit]
     v.setOnClickListener(new OnClickListener {
       override def onClick(v: View) { es fire Unit }
+    })
+    es
+  }
+
+  sealed trait TextWatcherEvent
+  case class BeforeTextChangedEvent(text: CharSequence, start: Int, count: Int, after: Int) extends TextWatcherEvent
+  case class TextChangedEvent(text: CharSequence, start: Int, before: Int, count: Int) extends TextWatcherEvent
+  case class AfterTextChangedEvent(text: Editable) extends TextWatcherEvent
+
+  def textChanges(tv: android.widget.TextView): EventStream[TextWatcherEvent] = {
+    val es = EventSource[TextWatcherEvent]
+    tv.addTextChangedListener(new TextWatcher {
+      override def beforeTextChanged(text: CharSequence, start: Int, count: Int, after: Int) {
+        es fire BeforeTextChangedEvent(text, start, count, after)
+      }
+      override def onTextChanged(text: CharSequence, start: Int, count: Int, after: Int) {
+        es fire TextChangedEvent(text, start, count, after)
+      }
+      override def afterTextChanged(text: Editable) {
+        es fire AfterTextChangedEvent(text)
+      }
     })
     es
   }
